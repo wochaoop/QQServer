bf77c10005efe16bd4f3debb61a5e29d0ad02577
xiwangly2 <1334850101@qq.om>
2023-06-09 19:09:23 +0800

    Socket

--- src/com/example/client/Client.java
+++ src/com/example/client/Client.java
@@ -1,19 +1,46 @@
 package com.example.client;
 
-import java.io.IOException;
-import java.net.Socket;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
         try {
+            // 设置目标Python服务器的URL
+            URL url = new URL("http://localhost:8000");
 
-            String host = "127.0.0.1";
+            while (true) {
+                // 读取用户的键盘输入
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter message: ");
+                String input = scanner.nextLine();
 
-            int port = 6666;
+                if (input.equals("exit")) {
+                    break;
+                }
 
-            Socket client = new Socket(host, port);
-            new WriteDateToServerThread(client).start();
-            new ReadDateFromServerThread(client).start();
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setDoOutput(true);
+
+                // 发送数据到Python服务器
+                OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+                writer.write(input);
+                writer.flush();
+
+                // 读取Python服务器的响应
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println("Response from Python: " + line);
+                }
+
+                writer.close();
+                reader.close();
+                connection.disconnect();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
